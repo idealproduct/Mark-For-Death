2f81a21a0be46b1b0b00d9de07c16908e66c553c
idealproduct <happberday@gmail.com>
2025-07-27 20:25:54 +0800

    hdewe

--- src/main/java/com/yichenxbohan/markedfordeath/event/SlowNearbyPlayersEvent.java
+++ src/main/java/com/yichenxbohan/markedfordeath/event/SlowNearbyPlayersEvent.java
@@ -3,6 +3,8 @@ package com.yichenxbohan.markedfordeath.event;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.BlockPos;
@@ -14,17 +16,17 @@ public class SlowNearbyPlayersEvent {
         // 建立範圍盒子（AABB）
         AABB area = new AABB(center).inflate(radius);
 
-        for (Player player : level.getEntitiesOfClass(Player.class, area)) {
-            if (player == source) continue;  // 排除自己
-            if (!player.hasEffect(MobEffects.MOVEMENT_SLOWDOWN)) {
-                player.addEffect(new MobEffectInstance(
-                        MobEffects.MOVEMENT_SLOWDOWN,
-                        60,
-                        1,
-                        false,
-                        true
-                ));
-            }
+        for (LivingEntity entity : level.getEntitiesOfClass(LivingEntity.class, area)) {
+            if (entity == source) continue;  // 排除自己
+
+            entity.addEffect(new MobEffectInstance(
+                    MobEffects.MOVEMENT_SLOWDOWN,
+                    100,
+                    6,
+                    false,
+                    false
+            ));
+
         }
 
     }
